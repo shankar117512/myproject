504078a24937714258afbb54a30a576148f0e5f9
shankar117512 <shankar117512@gmail.com>
2026-03-30 12:27:49 +0530

    urls

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from yourapp.views import home
+from config.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
