00a9494e369f60d253d64228ef696862492f3589
shankar117512 <shankar117512@gmail.com>
2026-03-30 12:03:38 +0530

    base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='change-me-in-production')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
