51181ebdf2b32cce166c770e0f59540485b057fc
shankar117512 <shankar117512@gmail.com>
2026-04-02 10:36:02 +0530

    url & views

--- config/views.py
+++ config/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def home(request):
     return HttpResponse("Django staging deployed successfully...! 🚀")
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
